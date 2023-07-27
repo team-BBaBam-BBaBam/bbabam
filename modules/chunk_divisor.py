20b985ef46b9cab11d86a6621fd7ffb2c28d53d9
gongbj <gongbj0113@gmail.com>
2023-07-27 16:21:53 +0900

    Chunk Divisor 개발함

---
+++ modules/chunk_divisor.py
@@ -0,0 +1,69 @@
+from typing import TypedDict, List, Tuple
+import tiktoken
+
+
+class Chunk(TypedDict):
+    text: str
+    token_count: int
+
+
+class ChunkDividedText(TypedDict):
+    chunks: List[Chunk]
+    total_token_count: int
+    link: str
+
+
+class RawText(TypedDict):
+    text: str
+    link: str
+
+
+class ChunkDivisor:
+
+    def __init__(self, isGpt3:bool=False):
+        self.enc = tiktoken.encoding_for_model(
+            "gpt-4" if not isGpt3 else "gpt-3.5-turbo")
+
+    def __split_text(self, text: str, chunk_size: int = 200, chunk_overlap: int = 20) -> List[Tuple[str, int]]:
+        '''
+        Split text into chunks of tokens, with a sliding window based on the chunk size and overlap.
+        text: Text to split
+        chunk_size: Maximum token count of each chunk
+        chunk_overlap: Overlap token count between chunks
+        '''
+        splits: List[Tuple[str, int]] = []
+
+        encoded = self.enc.encode(text)
+        start_idx = 0
+        cur_idx = min(start_idx + chunk_size, len(encoded))
+
+        cur_tokens = encoded[start_idx:cur_idx]
+        while start_idx < len(encoded):
+            splits.append((self.enc.decode(cur_tokens), len(cur_tokens)))
+            start_idx += chunk_size - chunk_overlap
+            cur_idx = min(start_idx + chunk_size, len(encoded))
+            cur_tokens = encoded[start_idx:cur_idx]
+
+        return splits
+
+    def divide_chunks(self, data: List[RawText], chunk_size: int = 200, chunk_overlap: int = 20) -> List[ChunkDividedText]:
+        '''
+        data: List of RawText
+        max_token_count: Maximum token count of each chunk
+        return: List of ChunkDividedText
+        '''
+        divided_texts = []
+
+        for raw_text in data:
+            chunks = []
+            for chunk, token_count in self.__split_text(raw_text["text"], chunk_size=chunk_size, chunk_overlap=chunk_overlap):
+                chunks.append({
+                    "text": chunk,
+                    "token_count": token_count
+                })
+            divided_texts.append({
+                "chunks": chunks,
+                "total_token_count": sum([chunk["token_count"] for chunk in chunks]),
+                "link": raw_text["link"]
+            })
+        return divided_texts
---
+++ modules/test/chunk_divisor_test.py
@@ -0,0 +1,52 @@
+# python .\modules\test\chunk_divisor_test.py
+
+if __name__ == "__main__":
+    import sys
+    import os
+    current = os.path.dirname(os.path.realpath(__file__))
+    parent = os.path.dirname(os.path.dirname(current))
+    sys.path.append(parent)
+
+from modules.chunk_divisor import ChunkDivisor
+
+from termcolor import colored
+
+
+def main():
+    # read text from bloc_content1.txt
+    with open('modules/test/blog_content1.txt', 'r', encoding='utf-8') as f:
+        blog1 = f.read()
+
+    data = [
+        {
+            "text": "Hello, I am a sample text for testing the ChunkDivisor class.",
+            "link": "https://example.com/sample1"
+        },
+        {
+            "text": blog1,
+            "link": "https://blog.naver.com/eun417911/223129682887"
+        }
+    ]
+
+    chunk_divisor = ChunkDivisor(isGpt3=False)
+
+    divided_texts = chunk_divisor.divide_chunks(data, chunk_size=200)
+
+    for text in divided_texts:
+        print("Link: " + colored(f"{text['link']}", 'green'))
+        print(
+            "Total Token Count: " + colored(f"{text['total_token_count']}", 'blue'))
+        print('')
+        i = 0
+        for chunk in text['chunks']:
+            print(colored(
+                f"Token Count: {chunk['token_count']}", 'yellow' if i % 2 == 0 else 'magenta'))
+            print(colored(f"Text: ", 'yellow' if i %
+                  2 == 0 else 'magenta') + chunk['text'])
+            i += 1
+
+        print('\n')
+
+
+if __name__ == "__main__":
+    main()
