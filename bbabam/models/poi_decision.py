aa73e85fc2c343c48845a702257bebf2c5b5110d
gongbj <gongbj0113@gmail.com>
2023-07-28 21:09:40 +0900

    폴더 구조, 파일 이름 등을 리팩토링 - 기존 main.py를 test/keyword_and_crawling_test.py로 이동하였습니다. - model에는 chatgpt 프롬프팅을 통한 모델을 분류하였고,  module에는 프롬프팅 이외의 모든 모듈 단위를 분류하였습니다. -  settings의 openailm.py를 openai_lm.py로 이름변경했습니다. - http_request_randomizer를 utils에 포함시켰습니다.
    폴더 구조, 파일 이름 등을 리팩토링
    - 기존 main.py를 test/keyword_and_crawling_test.py로 이동하였습니다.
    - model에는 chatgpt 프롬프팅을 통한 모델을 분류하였고,  module에는 프롬프팅 이외의 모든 모듈 단위를 분류하였습니다.
    -  settings의 openailm.py를 openai_lm.py로 이름변경했습니다.
    - http_request_randomizer를 utils에 포함시켰습니다.

---
+++ bbabam/models/poi_decision.py
@@ -0,0 +1,30 @@
+import bbabam.settings.openai_lm as lm
+import re
+
+class PoiDecisionMaker:
+    def __init__(self):
+        self.get_model_name = lm.GetModelName()
+        self.chatmodel = lm.OpenAIChatModel(self.get_model_name.model_val(model = 'gpt-3.5', stable = 'yes'))
+        print(self.chatmodel)
+    
+    def decision_making(self, keyword, user_input):
+        note = """You will be given "search keyword" and "request in natural language". 
+        This inputs might include the context of information user wants to know. 
+        You should decide whether POI information is required for this context. 
+        POI information is the detailed and static information such as address, operation time and categories of the place. 
+        You should output the answer with 1 or 0, 1 is when POI information is needed and 0 is not. 
+        And some notes here again. Your return has to include only integer, without any explanation. 
+        """
+
+        request_msg = [
+            {"role": "system", "content": note},
+            {"role": "user", "content": ("Search Keyword: " + keyword + "Request in natural language: " + user_input)}
+        ]
+
+        reply = self.chatmodel.get_reply(request_msg)['content']
+        number = int(re.sub(r'[^0-9]', '', reply))
+
+        if number:
+            return True
+        else:
+            return False
---
+++ bbabam/settings/openai_lm.py
@@ -0,0 +1,104 @@
+import openai
+
+from bbabam.settings.environments import OpenAIKeys
+from bbabam.settings.errors import NotProvidedModelError
+
+CHATGPT_3_MODEL = "gpt-3.5-turbo"
+CHATGPT_3_MODEL_16K = "gpt-3.5-turbo-16k"
+CHATGPT_3_MODEL_STABLE = "gpt-3.5-turbo-0613"
+CHATGPT_3_MODEL_16K_STABLE = "gpt-3.5-turbo-16k-0613"
+CHATGPT_4_MODEL = "gpt-4"
+CHATGPT_4_MODEL_32K = "gpt-4-32k"
+CHATGPT_4_MODEL_STABLE = "gpt-4-0613"
+CHATGPT_4_MODEL_32K_STABLE = "gpt-4-32k-0613"
+WORD_EMBEDDING_MODEL = "text-embedding-ada-002"
+
+
+class OpenAIChatModel: #밑에 모델을 편하게 선택할 수 있는 get_model_name 클래스가 있지만,
+                    #여기서도 바로 값을 넣어 api를 구동할 수 있음.
+                    #알맞은 모델이름을 넣고 클래스 선언.
+                    #함수들에 딕셔너리 형태의 메세지를 넣으면 api값을 받아옴.
+                    #API 호출시 필수 파라미터가 아닌 값들은 일단 주석처리해둠.
+    def __init__(self, model, functions=None, 
+                 function_call=None, temperature=None, 
+                 top_p=None, n=None, stream=None, stop=None,
+                 max_tokens=None, presence_penalty=None, 
+                 frequency_penalty=None, logit_bias=None, user=None):
+        openai_keys = OpenAIKeys()
+        openai.api_key = openai_keys.get_key()
+        self.model = model
+        # self.functions = functions
+        # self.function_call = function_call
+        # self.temperature = temperature, 
+        # self.top_p = top_p
+        # self.n = n
+        # self.stream = stream
+        # self.stop = stop
+        # self.max_tokens = max_tokens
+        # self.presence_penalty =  presence_penalty
+        # self.frequency_penalty = frequency_penalty
+        # self.logit_bias = logit_bias
+        # self.user = user
+
+    def get_available(self):
+        return openai.Model.list()
+
+    def get_completion(self, messages):
+        completion = openai.ChatCompletion.create(
+        model=self.model,
+        messages=messages
+        )
+        return completion
+    
+    def get_reply(self, messages):
+        return self.get_completion(messages).choices[0].message
+
+
+class OpenAIEmbeddingModel: #워드임베딩 api는 하나밖에 없으므로 모델명은 인풋으로 받지 않음.
+    # 인풋문장을 넣어주면 출력되는 get_embedding 함수가 있음.
+    def __init__(self):
+        openai_keys = OpenAIKeys()
+        openai.api_key = openai_keys.get_key()
+
+    def get_embeddings(self, input):
+        embeddings = openai.Embedding.create(
+            model="text-embedding-ada-002",
+            input=input
+        )
+        return embeddings
+    
+    def get_vector(self, input):
+        return self.get_embeddings(input).data[0].embedding
+
+
+class GetModelName: # 모델을 편하게 선택하기 위한 클래스
+                    # model의 가능한 인풋은 gpt-3.5, gpt-4, word-embedding 중에 하나.
+    def __init__(self):
+        pass
+    
+    def model_val(self, model, stable=None, more_tokens=None):
+            if model == 'gpt-3.5':
+                if stable is not None and more_tokens is not None:
+                    return CHATGPT_3_MODEL_16K_STABLE
+                elif stable is not None and more_tokens is None:
+                    return CHATGPT_3_MODEL_STABLE
+                elif stable is None and more_tokens is not None:
+                    return CHATGPT_3_MODEL_16K
+                else:
+                    return CHATGPT_3_MODEL
+                
+            elif model == 'gpt-4':
+                if stable is not None and more_tokens is not None:
+                    return CHATGPT_4_MODEL_32K_STABLE
+                elif stable is not None and more_tokens is None:
+                    return CHATGPT_4_MODEL_STABLE
+                elif stable is None and more_tokens is not None:
+                    return CHATGPT_4_MODEL_32K
+                else:
+                    return CHATGPT_4_MODEL
+                
+            elif model == 'word-embedding':
+                return WORD_EMBEDDING_MODEL
+
+            else:
+                raise NotProvidedModelError
