ab6d08d0302dcecaa1536f8d22847cbde1d5a98b
rzbsys <rzbsys@unist.ac.kr>
2023-08-05 17:15:26 +0900

    remove multiprocessing

--- bbabam/modules/crawling_module/crawling.py
+++ bbabam/modules/crawling_module/crawling.py
@@ -32,11 +32,11 @@ class SocialCrawl:
         return content
 
     def Multi_Crawler(self, query, txt_num, processor_num):
-        pool = mp.Pool(processes=processor_num)
+        # pool = mp.Pool(processes=processor_num)
         urls, tot_num = self.get_links(query, txt_num)
-        lst = pool.map(self.get_content, urls)
-        pool.close()
-        pool.join()
+        lst = list(map(self.get_content, urls))
+        # pool.close()
+        # pool.join()
         return lst, tot_num, urls
 
     def getTime(self, sec):
@@ -64,24 +64,38 @@ class SocialCrawl:
         s = time.time()
         self.searched_context = []
 
-        with Manager() as manager:
-            output_list = manager.list()  # Shared list
-            processes = []
-
-            for keyword in self.keywords:
-                p = Process(
-                    target=self.single_keyword_search,
-                    args=(keyword, txt_num, output_list),
+        for i in range(len(self.keywords)):
+            conts = []
+            cont_lst = []
+            conts, tot_num, urls = self.Multi_Crawler(
+                self.keywords[i], txt_num, int(txt_num / 2)
+            )
+            for j in range(len(conts)):
+                try:
+                    text = conts[j].replace("\u200b", "")
+                except:
+                    text = ""
+                cont_lst.append({"text": text, "link": urls[j]})
+            t = time.time()
+            times = (t - s) * (len(self.keywords) - i - 1) / (i + 1)
+            if on_print_message is not None:
+                on_print_message(
+                    f"{self.keywords[i]} Completed... ({i+1}/{len(self.keywords)}) | [Remaining time:{self.getTime(times)}]"
                 )
-                p.start()
-                processes.append(p)
-
-            for p in processes:
-                p.join()
-
-            self.output = list(output_list)  # Convert back to regular list
-
-            return self.output
+            else:
+                print(
+                    f"\r[{i+1}/{len(self.keywords)}]|"
+                    + self.keywords[i]
+                    + f"[{tot_num}] Completed...|[Remaining time:{self.getTime(times)}]",
+                    end="",
+                )
+            self.searched_context = {"keywords": self.keywords[i], "contents": cont_lst}
+            self.output.append(self.searched_context)
+            if self.proxy_activate:
+                time.sleep(1)
+            else:
+                time.sleep(5)
+        return self.output
 
 
 class POICrawl:
--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from server.app import socketio, app
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=80, debug=True)
+    socketio.run(app, host="0.0.0.0", port=81, debug=True)
